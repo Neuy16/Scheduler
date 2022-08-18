2a01be7bca1316147b6e0692f320e332904e3748
Neuy <neuwirth.andrew9@gmail.com>
2022-08-18 09:52:57 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// let now = moment();
+// now = document.getElementById('currentDay').innerHTML;
+//  create rows for days of the month
+
+
+function dayOfMonth() {
+    let days = document.createElement('div');
+
+
+}
+
+
+
+// Current day is always on top of the list
