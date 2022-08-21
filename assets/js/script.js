ad2c804f621f9529f7ff6e7425ef30c1655ad68c
Neuy <neuwirth.andrew9@gmail.com>
2022-08-20 22:34:20 -0400

    formatted JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,7 @@ let dailyEvent = '';
 let checkTime = moment().format('ha');
 let timeSlot = '';
 let timeColor = '';
-console.log(checkTime)
-console.log(hour)
+
 // Current date
 function setup() {
     var responseDate = moment().format('DD/MM/YYYY');
@@ -54,29 +53,3 @@ document.getElementById('saveBtn8').addEventListener('click', saveBtn);
 
 setup();
 displayEvent();
-
-
-
-// grab current time check against every block of time.... change color for time (put in setup)
-
-// Current day is always on top of the list
-
-// GIVEN I am using a daily planner to create a schedule
-
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-
-// WHEN I refresh the page
-// THEN the saved events persist
