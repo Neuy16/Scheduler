81795d5e6e56c4bbffa54d472195078f75971101
Neuy <neuwirth.andrew9@gmail.com>
2022-08-20 22:32:55 -0400

    implimented time coordinated time slots, finished README, finished save buttons and CSS

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Scheduler
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- As I start my new job, I decided to create this work day schedular to help with my time management. This will allow me to pace myself throughout the work day and create a list to work towards during the day. I've learned that I can stay on task better when I have small goals to work towards. Having a schedular lets me stay on task as well as show my employer my thought process for the day. 
+- This application solved my problem wandering off mentally and helps me stay on task, as well as giving me deadlines for certain tasks like completing tickets given to me during the work day.
+- By completing this word day schedular, I learned how to use moment.js to compare times during the day to change the colors of the text areas to indicate if my time has passed or if my task is still due.
+
+## Snippet
+
+![](assets/images/schedular.PNG)
+
+
+## Deployed application link
+
+https://neuy16.github.io/Scheduler/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,63 @@
 // let now = moment();
-// now = document.getElementById('currentDay').innerHTML;
 //  create rows for days of the month
-let hour = ['9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm']
-
-function dayOfMonth() {
+let hour = [moment().set({"hour": 9}).format('ha'), moment().set({"hour": 10}).format('ha'), moment().set({"hour": 11}).format('ha'), moment().set({"hour": 12}).format('ha'), moment().set({"hour": 13}).format('ha'), moment().set({"hour": 14}).format('ha'), moment().set({"hour": 15}).format('ha'), moment().set({"hour": 16}).format('ha'), moment().set({"hour": 17}).format('ha')];
+let dailyEvent = '';
+let checkTime = moment().format('ha');
+let timeSlot = '';
+let timeColor = '';
+console.log(checkTime)
+console.log(hour)
+// Current date
+function setup() {
     var responseDate = moment().format('DD/MM/YYYY');
     let today = document.getElementById('currentDay');
     today.textContent = responseDate;
+    
+    // Hour in the day (9-5)
+    for (let t = 0; t < hour.length; t++) {
+        timeSlot = document.getElementById(t);
+        timeSlot.innerHTML = hour[t];
+        timeColor = $('#dailyEvent' + t);
+        if (hour[t] == checkTime) {
+            timeColor.addClass('present');
+        } else if (moment(hour[t], 'ha') < checkTime) {
+            timeColor.addClass('past');
+        } else {
+            timeColor.addClass('future');
+        }
+    };
 }
-dayOfMonth();
 
-for (t=0; t<hour.length; t++) {
-    let timeSlot = document.getElementById(t);
-    timeSlot.innerHTML = hour[t];
-};
+// Button to save daily events
+function saveBtn() {
+    for (let i = 0; i < hour.length; i++) {
+        dailyEvent = $('#dailyEvent' + i).val();
+        localStorage.setItem(i, dailyEvent);
+    }
+}
 
-let dailyEvent = document.getElementsByTagName('textarea');
+function displayEvent() {
+    for (let i = 0; i < hour.length; i++) {
+        dailyEvent = $('#dailyEvent' + i).val(localStorage[i]);
+    }
+}
+
+document.getElementById('saveBtn0').addEventListener('click', saveBtn);
+document.getElementById('saveBtn1').addEventListener('click', saveBtn);
+document.getElementById('saveBtn2').addEventListener('click', saveBtn);
+document.getElementById('saveBtn3').addEventListener('click', saveBtn);
+document.getElementById('saveBtn4').addEventListener('click', saveBtn);
+document.getElementById('saveBtn5').addEventListener('click', saveBtn);
+document.getElementById('saveBtn6').addEventListener('click', saveBtn);
+document.getElementById('saveBtn7').addEventListener('click', saveBtn);
+document.getElementById('saveBtn8').addEventListener('click', saveBtn);
+
+setup();
+displayEvent();
+
+
+
+// grab current time check against every block of time.... change color for time (put in setup)
 
 // Current day is always on top of the list
 
